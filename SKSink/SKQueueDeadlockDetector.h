37237902d06c7aacbdd730547cf48dc75e899c65
Alexander Skorulis <skorulis@gmail.com>
2016-01-11 18:17:09 +1100

    Add SKQueueDeadlockDetector

---
+++ SKSink/SKQueueDeadlockDetector.h
@@ -0,0 +1,12 @@
+//  Created by Alexander Skorulis on 18/08/2015.
+//  Copyright (c) 2015 com.skorulis. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+@interface SKQueueDeadlockDetector : NSObject
+
+- (void) takeLock:(NSThread*)thread;
+- (void) releaseLock:(NSThread*)thread;
+
+
+@end
